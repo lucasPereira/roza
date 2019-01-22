f9408f71a07c52d69b0480dc28417b4070f96b90
Lucas Pereira da Silva <lucas.pereira@ufsc.br>
2019-01-22 17:24:01 -0200

    Extracting test cases from classes

---
+++ test/br/ufsc/ine/leb/roza/extractor/Junit4TestCaseExtractorTest.java
@@ -0,0 +1,35 @@
+package br.ufsc.ine.leb.roza.extractor;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import br.ufsc.ine.leb.roza.TestCase;
+import br.ufsc.ine.leb.roza.TestClass;
+import br.ufsc.ine.leb.roza.TestMethod;
+
+public class Junit4TestCaseExtractorTest {
+
+	private TestCaseExtractor extractor;
+
+	@BeforeEach
+	void setup() {
+		extractor = new Junit4TestCaseExtractor();
+	}
+
+	@Test
+	void oneTestMethodWithoutFixturesOrAsserts() throws Exception {
+		TestMethod testMethod = new TestMethod("example", Arrays.asList());
+		TestClass testClass = new TestClass("Example", Arrays.asList(), Arrays.asList(testMethod));
+		List<TestCase> testCases = extractor.extract(Arrays.asList(testClass));
+		assertEquals(1, testCases.size());
+		assertEquals("example", testCases.get(0).getName());
+		assertEquals(0, testCases.get(0).getFixtures().size());
+		assertEquals(0, testCases.get(0).getAsserts().size());
+	}
+
+}
