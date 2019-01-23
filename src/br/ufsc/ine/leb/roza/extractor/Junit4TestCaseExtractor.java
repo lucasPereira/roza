d9a20353286564ab54edeb519e0998f7283af37f
Lucas Pereira da Silva <lucas.pereira@ufsc.br>
2019-01-23 19:26:31 -0200

    Distinguishes between fixtures and asserts

--- src/br/ufsc/ine/leb/roza/extractor/Junit4TestCaseExtractor.java
+++ src/br/ufsc/ine/leb/roza/extractor/Junit4TestCaseExtractor.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
+import br.ufsc.ine.leb.roza.Statement;
 import br.ufsc.ine.leb.roza.TestCase;
 import br.ufsc.ine.leb.roza.TestClass;
 
@@ -12,9 +13,21 @@ public class Junit4TestCaseExtractor implements TestCaseExtractor {
 	@Override
 	public List<TestCase> extract(List<TestClass> testClasses) {
 		List<TestCase> testCases = new LinkedList<>();
-		TestCase testCase = new TestCase(testClasses.get(0).getTestMethods().get(0).getName(), Arrays.asList(), Arrays.asList());
+		String name = testClasses.get(0).getTestMethods().get(0).getName();
+		List<Statement> statements = testClasses.get(0).getTestMethods().get(0).getStatements();
+		List<Statement> fixtures = new LinkedList<>();
+		List<Statement> assertions = new LinkedList<>();
+		statements.forEach((statement) -> {
+			List<Statement> bucket = (statementIsAssertion(statement)) ? assertions : fixtures;
+			bucket.add(statement);
+		});
+		TestCase testCase = new TestCase(name, Arrays.asList(), assertions);
 		testCases.add(testCase);
 		return testCases;
+	}
+
+	private Boolean statementIsAssertion(Statement statement) {
+		return false;
 	}
 
 }
