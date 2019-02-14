3dbc7205aeb74fbba05c5bc95681a1809c32ea56
Lucas Pereira da Silva <lucas.pereira@ufsc.br>
2019-02-14 13:48:57 -0200

    Materializer returns materializations

---
+++ src/br/ufsc/ine/leb/roza/TestCaseMaterialization.java
@@ -0,0 +1,27 @@
+package br.ufsc.ine.leb.roza;
+
+import java.io.File;
+
+public class TestCaseMaterialization {
+
+	private File file;
+	private TestCase testCase;
+
+	public TestCaseMaterialization(File file, TestCase testCase) {
+		this.file = file;
+		this.testCase = testCase;
+	}
+
+	public String getFileName() {
+		return file.getName();
+	}
+
+	public String getFilePath() {
+		return file.getPath();
+	}
+
+	public TestCase getTestCase() {
+		return testCase;
+	}
+
+}
