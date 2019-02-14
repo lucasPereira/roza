cff4d3051785a83a00956f3df57c5a37c7d98819
Lucas Pereira da Silva <lucas.pereira@ufsc.br>
2019-02-14 13:52:00 -0200

    Assessment of similarity through JPlag

--- src/br/ufsc/ine/leb/roza/measurer/JplagSimilarityMeasurer.java
+++ src/br/ufsc/ine/leb/roza/measurer/JplagSimilarityMeasurer.java
@@ -1,25 +1,114 @@
 package br.ufsc.ine.leb.roza.measurer;
 
+import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import br.ufsc.ine.leb.roza.SimilarityAssessment;
 import br.ufsc.ine.leb.roza.SimilarityReport;
 import br.ufsc.ine.leb.roza.TestCase;
+import br.ufsc.ine.leb.roza.TestCaseMaterialization;
+import br.ufsc.ine.leb.roza.materializer.OneTestCasePerClassTestCaseMaterializer;
+import br.ufsc.ine.leb.roza.materializer.TestCaseMaterializer;
+import jplag.ExitException;
+import jplag.Program;
+import jplag.options.CommandLineOptions;
 
 public class JplagSimilarityMeasurer implements SimilarityMeasurer {
 
+	private String gerenatedSourceFolder;
+	private String resultsFolder;
+	private TestCaseMaterializer materializer;
+
+	public JplagSimilarityMeasurer(String generatedSourceFolder, String resultsFolder) {
+		this.gerenatedSourceFolder = generatedSourceFolder;
+		this.resultsFolder = resultsFolder;
+		materializer = new OneTestCasePerClassTestCaseMaterializer(generatedSourceFolder);
+	}
+
 	@Override
 	public SimilarityReport measure(List<TestCase> testCases) {
+		Map<TestCase, Map<TestCase, BigDecimal>> scores = new HashMap<>();
+		List<TestCaseMaterialization> materializations = materializer.materialize(testCases);
+		if (testCases.size() > 1) {
+			runJplag();
+			Map<TestCase, Map<TestCase, BigDecimal>> jplagScores = parserJplagReport(materializations);
+			scores.putAll(jplagScores);
+		}
 		List<SimilarityAssessment> assesssments = new LinkedList<>();
 		for (TestCase source : testCases) {
 			for (TestCase target : testCases) {
-				SimilarityAssessment assessment = new SimilarityAssessment(source, target, BigDecimal.ONE);
+				BigDecimal score = evaluateScore(scores, source, target);
+				SimilarityAssessment assessment = new SimilarityAssessment(source, target, score);
 				assesssments.add(assessment);
 			}
 		}
 		return new SimilarityReport(assesssments);
+	}
+
+	private Map<TestCase, Map<TestCase, BigDecimal>> parserJplagReport(List<TestCaseMaterialization> materializations) {
+		try {
+			Map<String, TestCase> testCases = new HashMap<>();
+			Map<TestCase, Map<TestCase, BigDecimal>> scores = new HashMap<>();
+			for (TestCaseMaterialization materialization : materializations) {
+				testCases.put(materialization.getFileName(), materialization.getTestCase());
+				scores.put(materialization.getTestCase(), new HashMap<>());
+			}
+			Document document = Jsoup.parse(new File(resultsFolder, "index.html"), "utf-8");
+			Elements tables = document.body().getElementsByTag("table");
+			Element table = tables.get(2);
+			Elements rows = table.getElementsByTag("tr");
+			for (Integer rowIndex = 0; rowIndex < rows.size(); rowIndex++) {
+				Element row = rows.get(0);
+				Elements columns = row.getElementsByTag("td");
+				Element source = columns.get(0);
+				String sourceName = source.text();
+				for (Integer columnIndex = 2; columnIndex < columns.size(); columnIndex++) {
+					Element target = columns.get(columnIndex);
+					String targetName = target.getElementsByTag("a").get(0).text();
+					String textScore = target.getElementsByTag("font").get(0).text().replaceAll("[^0-9]", "");
+					BigDecimal score = new BigDecimal(textScore).divide(new BigDecimal(1000));
+					TestCase targetTestCase = testCases.get(targetName);
+					TestCase sourceTestCase = testCases.get(sourceName);
+					scores.get(sourceTestCase).put(targetTestCase, score);
+					scores.get(targetTestCase).put(sourceTestCase, score);
+				}
+			}
+			return scores;
+		} catch (IOException excecao) {
+			throw new RuntimeException(excecao);
+		}
+	}
+
+	private BigDecimal evaluateScore(Map<TestCase, Map<TestCase, BigDecimal>> scores, TestCase source, TestCase target) {
+		if (source.equals(target)) {
+			return BigDecimal.ONE;
+		} else if (scores.containsKey(source) && scores.get(source).containsKey(target)) {
+			return scores.get(source).get(target);
+		} else {
+			return BigDecimal.ZERO;
+		}
+	}
+
+	private void runJplag() {
+		try {
+			String logFile = new File(resultsFolder, "log.txt").getAbsolutePath();
+			String[] arguments = { "-l", "java17", "-vlpd", "-t", "5", "-m", "0%", "-s", gerenatedSourceFolder, "-r", resultsFolder, "-o", logFile };
+			CommandLineOptions options = new CommandLineOptions(arguments, null);
+			Program program = new Program(options);
+			program.run();
+		} catch (ExitException excecao) {
+			throw new RuntimeException(excecao);
+		}
 	}
 
 }
--- test/br/ufsc/ine/leb/roza/measurer/JplagSimilarityMeasurerTest.java
+++ test/br/ufsc/ine/leb/roza/measurer/JplagSimilarityMeasurerTest.java
@@ -1,34 +1,40 @@
 package br.ufsc.ine.leb.roza.measurer;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.ufsc.ine.leb.roza.SimilarityReport;
 import br.ufsc.ine.leb.roza.Statement;
 import br.ufsc.ine.leb.roza.TestCase;
-import br.ufsc.ine.leb.roza.measurer.JplagSimilarityMeasurer;
-import br.ufsc.ine.leb.roza.measurer.SimilarityMeasurer;
+import br.ufsc.ine.leb.roza.utils.FolderUtils;
 
 public class JplagSimilarityMeasurerTest {
 
+	private JplagSimilarityMeasurer measurer;
+
+	@BeforeEach
+	void setup() {
+		new FolderUtils("test-resources/materializer").createEmptyFolder();
+		new FolderUtils("test-resources/similarity").createEmptyFolder();
+		measurer = new JplagSimilarityMeasurer("test-resources/materializer", "test-resources/similarity");
+	}
+
 	@Test
 	void zeroTestCases() throws Exception {
-		SimilarityMeasurer measurer = new JplagSimilarityMeasurer();
 		SimilarityReport report = measurer.measure(Arrays.asList());
-
 		assertEquals(0, report.getAssessments().size());
 	}
 
 	@Test
 	void oneTestCase() throws Exception {
 		Statement fixture = new Statement("sut(0);");
-		Statement assertion = new Statement("assertEquals(0, 0)");
+		Statement assertion = new Statement("assertEquals(0, 0);");
 		TestCase testCase = new TestCase("test", Arrays.asList(fixture), Arrays.asList(assertion));
-		SimilarityMeasurer measurer = new JplagSimilarityMeasurer();
 		SimilarityReport report = measurer.measure(Arrays.asList(testCase));
 
 		assertEquals(1, report.getAssessments().size());
@@ -40,10 +46,9 @@ public class JplagSimilarityMeasurerTest {
 	@Test
 	void twoIdenticalTestCases() throws Exception {
 		Statement fixture = new Statement("sut(0);");
-		Statement assertion = new Statement("assertEquals(0, 0)");
+		Statement assertion = new Statement("assertEquals(0, 0);");
 		TestCase testCaseA = new TestCase("test", Arrays.asList(fixture), Arrays.asList(assertion));
 		TestCase testCaseB = new TestCase("test", Arrays.asList(fixture), Arrays.asList(assertion));
-		SimilarityMeasurer measurer = new JplagSimilarityMeasurer();
 		SimilarityReport report = measurer.measure(Arrays.asList(testCaseA, testCaseB));
 
 		assertEquals(4, report.getAssessments().size());
@@ -54,6 +59,29 @@ public class JplagSimilarityMeasurerTest {
 		assertEquals(testCaseA, report.getAssessments().get(1).getSource());
 		assertEquals(testCaseB, report.getAssessments().get(1).getTarget());
 		assertEquals(BigDecimal.ONE, report.getAssessments().get(2).getScore());
+		assertEquals(testCaseB, report.getAssessments().get(2).getSource());
+		assertEquals(testCaseA, report.getAssessments().get(2).getTarget());
+		assertEquals(BigDecimal.ONE, report.getAssessments().get(3).getScore());
+		assertEquals(testCaseB, report.getAssessments().get(3).getSource());
+		assertEquals(testCaseB, report.getAssessments().get(3).getTarget());
+	}
+
+	@Test
+	void twoDistinctTestCases() throws Exception {
+		Statement fixture = new Statement("new Sut(0).sut();");
+		Statement assertion = new Statement("assertEquals(0, 0);");
+		TestCase testCaseA = new TestCase("testA", Arrays.asList(fixture), Arrays.asList());
+		TestCase testCaseB = new TestCase("testB", Arrays.asList(fixture), Arrays.asList(assertion));
+		SimilarityReport report = measurer.measure(Arrays.asList(testCaseA, testCaseB));
+
+		assertEquals(4, report.getAssessments().size());
+		assertEquals(BigDecimal.ONE, report.getAssessments().get(0).getScore());
+		assertEquals(testCaseA, report.getAssessments().get(0).getSource());
+		assertEquals(testCaseA, report.getAssessments().get(0).getTarget());
+		assertEquals(new BigDecimal("0.705"), report.getAssessments().get(1).getScore());
+		assertEquals(testCaseA, report.getAssessments().get(1).getSource());
+		assertEquals(testCaseB, report.getAssessments().get(1).getTarget());
+		assertEquals(new BigDecimal("0.705"), report.getAssessments().get(2).getScore());
 		assertEquals(testCaseB, report.getAssessments().get(2).getSource());
 		assertEquals(testCaseA, report.getAssessments().get(2).getTarget());
 		assertEquals(BigDecimal.ONE, report.getAssessments().get(3).getScore());
