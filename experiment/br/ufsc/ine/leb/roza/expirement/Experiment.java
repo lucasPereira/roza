9ef37bb38b1aad07fba685a5d59de5c4b500e24e
Lucas Pereira da Silva <lucas.pereira@ufsc.br>
2019-05-15 17:05:25 -0300

    Running JPlag experiment and refactors

--- experiment/br/ufsc/ine/leb/roza/expirement/Experiment.java
+++ experiment/br/ufsc/ine/leb/roza/expirement/Experiment.java
@@ -16,9 +16,11 @@ import br.ufsc.ine.leb.roza.loader.RecursiveTextFileLoader;
 import br.ufsc.ine.leb.roza.loader.TextFileLoader;
 import br.ufsc.ine.leb.roza.materializer.Junit4WithoutAssertionsTestCaseMaterializer;
 import br.ufsc.ine.leb.roza.materializer.TestCaseMaterializer;
+import br.ufsc.ine.leb.roza.measurer.JplagSimilarityMeasurer;
 import br.ufsc.ine.leb.roza.measurer.LccssSimilarityMeasurer;
 import br.ufsc.ine.leb.roza.measurer.SimianSimilarityMeasurer;
 import br.ufsc.ine.leb.roza.measurer.SimilarityMeasurer;
+import br.ufsc.ine.leb.roza.measurer.configuration.jplag.JplagConfigurations;
 import br.ufsc.ine.leb.roza.measurer.configuration.simian.SimianConfigurations;
 import br.ufsc.ine.leb.roza.measurer.report.AssessmentTestCaseNameComparator;
 import br.ufsc.ine.leb.roza.parser.Junit5TestClassParser;
@@ -31,6 +33,7 @@ public class Experiment {
 	public static void main(String[] args) {
 		GroundTruth groundTruth = new GroundTruth();
 		groundTruth.findInconsistences();
+		new FolderUtils("experiment-results").createEmptyFolder();
 		lccss();
 		simian(2);
 		simian(3);
@@ -41,47 +44,52 @@ public class Experiment {
 		simian(8);
 		simian(9);
 		simian(10);
+		jplag(1);
+		jplag(2);
+		jplag(3);
+		jplag(4);
+		jplag(5);
+		jplag(6);
+		jplag(7);
+		jplag(8);
+		jplag(9);
+		jplag(10);
+		jplag(11);
 	}
 
 	private static void lccss() {
-		new FolderUtils("execution/materializer").createEmptyFolder();
-		FolderUtils folderUtils = new FolderUtils("experiment-results");
-		TextFileLoader loader = new RecursiveTextFileLoader("experiment-resources");
-		TestClassParser parser = new Junit5TestClassParser();
-		TestCaseExtractor extractor = new JunitTestCaseExtractor(Arrays.asList("assegurarTexto", "assegurarValor", "assegurarQuantidadeDeElementos", "assegurarConteudoDeArquivoBaixado", "assegurarNaoMarcado", "assegurarMarcado", "assegurarMarcacao"));
-		TestCaseMaterializer materializer = new Junit4WithoutAssertionsTestCaseMaterializer("execution/materializer");
-		SimilarityMeasurer measurer = new LccssSimilarityMeasurer();
-		CommaSeparatedValues csv = new CommaSeparatedValues();
-
-		List<TextFile> files = loader.load();
-		List<TestClass> testClasses = parser.parse(files);
-		List<TestCase> testCases = extractor.extract(testClasses);
-		MaterializationReport materializationReport = materializer.materialize(testCases);
-		SimilarityReport similarityReport = measurer.measure(materializationReport);
-		similarityReport.sort(new AssessmentTestCaseNameComparator());
-		List<SimilarityAssessment> assessments = similarityReport.getAssessments();
-		DecimalFormat scoreFormatter = new DecimalFormat();
-		scoreFormatter.setMaximumFractionDigits(Integer.MAX_VALUE);
-		for (SimilarityAssessment assessment : assessments) {
-			String sourceName = assessment.getSource().getName();
-			String targetName = assessment.getTarget().getName();
-			String score = scoreFormatter.format(assessment.getScore());
-			csv.addLine(sourceName, targetName, score);
-		}
 		String fileName = "lccss.csv";
-		folderUtils.writeContetAsString(fileName, csv.getContent());
+		SimilarityMeasurer measurer = new LccssSimilarityMeasurer();
+		measure(measurer, fileName);
 	}
 
 	private static void simian(Integer threshold) {
+		String fileName = String.format("simian-%d.csv", threshold);
+		SimianConfigurations configurations = new SimianConfigurations().threshold(threshold);
+		SimilarityMeasurer measurer = new SimianSimilarityMeasurer(configurations, "execution/measurer");
+		measure(measurer, fileName);
+	}
+
+	private static void jplag(Integer sensitivity) {
+		String fileName = String.format("jplag-%d.csv", sensitivity);
+		JplagConfigurations configurations = new JplagConfigurations().sensitivity(sensitivity).sources("execution/materializer").results("execution/measurer");
+		SimilarityMeasurer measurer = new JplagSimilarityMeasurer(configurations);
+		measure(measurer, fileName);
+	}
+
+	private static void measure(SimilarityMeasurer measurer, String fileName) {
 		new FolderUtils("execution/materializer").createEmptyFolder();
+		new FolderUtils("execution/measurer").createEmptyFolder();
+
 		FolderUtils folderUtils = new FolderUtils("experiment-results");
+		CommaSeparatedValues csv = new CommaSeparatedValues();
+		DecimalFormat scoreFormatter = new DecimalFormat();
+		scoreFormatter.setMaximumFractionDigits(Integer.MAX_VALUE);
+
 		TextFileLoader loader = new RecursiveTextFileLoader("experiment-resources");
 		TestClassParser parser = new Junit5TestClassParser();
 		TestCaseExtractor extractor = new JunitTestCaseExtractor(Arrays.asList("assegurarTexto", "assegurarValor", "assegurarQuantidadeDeElementos", "assegurarConteudoDeArquivoBaixado", "assegurarNaoMarcado", "assegurarMarcado", "assegurarMarcacao"));
 		TestCaseMaterializer materializer = new Junit4WithoutAssertionsTestCaseMaterializer("execution/materializer");
-		SimianConfigurations configurations = new SimianConfigurations().threshold(threshold);
-		SimilarityMeasurer measurer = new SimianSimilarityMeasurer(configurations, "execution/measurer");
-		CommaSeparatedValues csv = new CommaSeparatedValues();
 
 		List<TextFile> files = loader.load();
 		List<TestClass> testClasses = parser.parse(files);
@@ -90,15 +98,13 @@ public class Experiment {
 		SimilarityReport similarityReport = measurer.measure(materializationReport);
 		similarityReport.sort(new AssessmentTestCaseNameComparator());
 		List<SimilarityAssessment> assessments = similarityReport.getAssessments();
-		DecimalFormat scoreFormatter = new DecimalFormat();
-		scoreFormatter.setMaximumFractionDigits(Integer.MAX_VALUE);
+
 		for (SimilarityAssessment assessment : assessments) {
 			String sourceName = assessment.getSource().getName();
 			String targetName = assessment.getTarget().getName();
 			String score = scoreFormatter.format(assessment.getScore());
 			csv.addLine(sourceName, targetName, score);
 		}
-		String fileName = String.format("simian-%d.csv", threshold);
 		folderUtils.writeContetAsString(fileName, csv.getContent());
 	}
 
