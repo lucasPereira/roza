a61ff50f195ab884fa0deb6b928de9627c1f3e0f
Lucas Pereira da Silva <lucas.pereira@ufsc.br>
2020-06-25 15:19:22 -0300

    Adds method to generate fraction based big decimal

--- main/src/br/ufsc/ine/leb/roza/utils/MathUtils.java
+++ main/src/br/ufsc/ine/leb/roza/utils/MathUtils.java
@@ -5,12 +5,20 @@ import java.math.MathContext;
 
 public class MathUtils {
 
-	public BigDecimal oneOver(Integer ...values) {
+	public BigDecimal oneOver(Integer... values) {
 		BigDecimal sum = BigDecimal.ZERO;
-		for (Integer value : values ) {
+		for (Integer value : values) {
 			sum = sum.add(new BigDecimal(value));
 		}
 		return BigDecimal.ONE.divide(sum, new MathContext(1000));
+	}
+
+	public BigDecimal over(Integer numerator, Integer... denominators) {
+		BigDecimal sum = BigDecimal.ZERO;
+		for (Integer value : denominators) {
+			sum = sum.add(new BigDecimal(value));
+		}
+		return new BigDecimal(numerator).divide(sum, new MathContext(1000));
 	}
 
 }
